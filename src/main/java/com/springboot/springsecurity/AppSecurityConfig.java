90ae8fb7b62bc8b582698b872094ec162fb1f29f
saikumar <saikumarkommineni123@gmail.com>
2024-07-19 18:34:17 +0530

    Initial commit

---
+++ src/main/java/com/springboot/springsecurity/AppSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.springboot.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+//@EnableOAuth2Sso
+public class AppSecurityConfig{
+	
+	//WebSecurity is used for secure the app using login username and password by using login page
+	//OAuth2 code is for login through the third party applications like Google,faceBook..., But for that we need to configure in properties file
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return provider;
+	}
+
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeRequests().requestMatchers("/login").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login").permitAll()
+		.and()
+		.logout().invalidateHttpSession(true)
+		.clearAuthentication(true)
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/logout-success").permitAll();
+		
+		//http.authenticationProvider(authProvider());
+		return http.build();
+	}
+	
+}
---
+++ src/main/java/com/springboot/springsecurity/UserPrinciple.java
@@ -0,0 +1,37 @@
+package com.springboot.springsecurity;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrinciple implements UserDetails {
+
+	@Autowired
+	private User user;
+
+	public UserPrinciple(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+}
